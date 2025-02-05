579a06c74e282d8af88b95e1b4ca2165f9295d00
Ajeet Kumar <74969795+ajeetkumar36698@users.noreply.github.com>
2025-02-05 20:47:41 +0530

    Update email_sending_python.py
    this is first commit

--- email_sending_python.py
+++ email_sending_python.py
@@ -3,8 +3,8 @@ server=smtplib.SMTP('smtp.gmail.com',587)
 server.starttls()
 
 
-server.login("ajeetkumar36697@gmail.com","votn sjtf ubzm mvqi")
-a=["ajeetkumar36698@gmail.com"]
-for i in range(len(a)):
-	server.sendmail("ajeetkumar36697@gmail.com",a[i],"if you get this message please replay any message")
+server.login("email","votn sjtf ubzm mvqi")
+
+
+server.sendmail("email",'sending email',"if you get this message please replay any message")
 print("email sending")
