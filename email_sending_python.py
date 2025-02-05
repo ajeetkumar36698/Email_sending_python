c5c5eb8a182dd3034a0acc07f22441569a38fe4c
ajeetkumar36698 <ajeetkumar36698@gmail.com>
2025-02-05 20:38:39 +0530

    first commit

---
+++ email_sending_python.py
@@ -0,0 +1,10 @@
+import smtplib
+server=smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+
+
+server.login("ajeetkumar36697@gmail.com","votn sjtf ubzm mvqi")
+a=['pradeeppatel622598@gmail.com',"ajeetkumar36698@gmail.com"]
+for i in range(len(a)):
+	server.sendmail("ajeetkumar36697@gmail.com",a[i],"if you get this message please replay any message")
+	print("email sending")
